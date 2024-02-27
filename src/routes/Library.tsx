100c2e61e72ceca64619da841fb8634cc2c1432c
Naffy Dharni <naffydharni006@gmail.com>
2024-02-28 01:18:11 +0530

    Add filter menu

--- src/routes/Library.tsx
+++ src/routes/Library.tsx
@@ -1,12 +1,19 @@
-import { Button, Input } from '@nextui-org/react'
+import {
+	Button,
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownTrigger,
+	Input,
+} from '@nextui-org/react'
 import { LibraryItems } from '../components/LibraryItems'
 import { useState } from 'react'
 
 const Library = () => {
-	const [query, setQuery] = useState("")
+	const [query, setQuery] = useState('')
 	return (
 		<>
-			<div className='flex flex-col w-full items-center p-2 overflow-auto'>
+			<div className='flex flex-col w-full items-center p-4 overflow-auto'>
 				<div className='flex flex-row w-full max-w-screen-sm gap-2'>
 					<Input
 						placeholder='Search...'
@@ -19,21 +26,37 @@ const Library = () => {
 						onValueChange={setQuery}
 						onClear={() => setQuery('')}
 					/>
-					<Button isIconOnly variant='flat' size='lg'>
-						<svg
-							width='24'
-							height='24'
-							fill='none'
-							className='fill-current'
-							viewBox='0 0 24 24'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								d='M10 16h4a1 1 0 0 1 .117 1.993L14 18h-4a1 1 0 0 1-.117-1.993L10 16h4-4Zm-2-5h8a1 1 0 0 1 .117 1.993L16 13H8a1 1 0 0 1-.117-1.993L8 11h8-8ZM5 6h14a1 1 0 0 1 .117 1.993L19 8H5a1 1 0 0 1-.117-1.993L5 6h14H5Z'
-								// fill='#fff'
-							/>
-						</svg>
-					</Button>
+					<Dropdown>
+						<DropdownTrigger>
+							<Button isIconOnly variant='flat' size='lg'>
+								<svg
+									width='24'
+									height='24'
+									fill='none'
+									className='fill-current'
+									viewBox='0 0 24 24'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										d='M10 16h4a1 1 0 0 1 .117 1.993L14 18h-4a1 1 0 0 1-.117-1.993L10 16h4-4Zm-2-5h8a1 1 0 0 1 .117 1.993L16 13H8a1 1 0 0 1-.117-1.993L8 11h8-8ZM5 6h14a1 1 0 0 1 .117 1.993L19 8H5a1 1 0 0 1-.117-1.993L5 6h14H5Z'
+										// fill='#fff'
+									/>
+								</svg>
+							</Button>
+						</DropdownTrigger>
+						<DropdownMenu aria-label='Static Actions'>
+							<DropdownItem key='last-read'>Last read</DropdownItem>
+							<DropdownItem key='latest-chapter'>Latest chapter</DropdownItem>
+							<DropdownItem key='edit'>Edit file</DropdownItem>
+							<DropdownItem
+								key='delete'
+								className='text-danger'
+								color='danger'
+							>
+								Delete file
+							</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
 					<Button isIconOnly variant='flat' size='lg'>
 						<svg
 							width='24'
