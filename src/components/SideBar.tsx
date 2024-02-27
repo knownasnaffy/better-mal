ff16e48110d9be867e6905e5e7a23c3b8cfb2d0d
Naffy Dharni <naffydharni006@gmail.com>
2024-02-28 00:43:05 +0530

    Add tooltip

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,39 +1,53 @@
-import { Tabs, Tab } from '@nextui-org/react';
-import { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { sideBarItems } from '../routes/Root';
+import { Tabs, Tab, Tooltip } from '@nextui-org/react'
+import { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { sideBarItems } from '../routes/Root'
 
 export function SideBar() {
-	const location = useLocation();
-	const navigate = useNavigate();
-	const [route, setRoute] = useState(location.pathname);
-	return <nav className='relative pt-2 w-fit'>
-		<Tabs
-			size='lg'
-			aria-label='Navigation'
-			selectedKey={route}
-			onSelectionChange={(key) => {
-				navigate(key.toString());
-				setRoute(key.toString());
-			}}
-			items={sideBarItems}
-			color='primary'
-			variant='light'
-			keyboardActivation='manual'
-			classNames={{
-				tab: 'justify-start',
-			}}
-			className='-mt-1 mx-1'
-		>
-			{(item) => {
-				return (
-					<Tab
-						key={item.url}
-						title={<div className='flex flex-row gap-2'>
-							{item.icon}
-						</div>} />
-				);
-			}}
-		</Tabs>
-	</nav>;
+	const location = useLocation()
+	const navigate = useNavigate()
+	const [route, setRoute] = useState(location.pathname)
+	return (
+		<nav className='relative pt-2 w-fit'>
+			<Tabs
+				size='lg'
+				aria-label='Navigation'
+				selectedKey={route}
+				onSelectionChange={(key) => {
+					navigate(key.toString())
+					setRoute(key.toString())
+				}}
+				items={sideBarItems}
+				color='primary'
+				variant='light'
+				keyboardActivation='manual'
+				classNames={{
+					// tab: 'justify-start',.
+					tab: 'p-0',
+				}}
+				className='-mt-1 mx-1'
+			>
+				{(item) => {
+					return (
+						<Tab
+							key={item.url}
+							title={
+								<Tooltip
+									content={item.title}
+									delay={500}
+									placement='right'
+									color='foreground'
+									// showArrow
+								>
+									<div className='flex size-full flex-row gap-2 py-1 px-3'>
+										{item.icon}
+									</div>
+								</Tooltip>
+							}
+						/>
+					)
+				}}
+			</Tabs>
+		</nav>
+	)
 }
