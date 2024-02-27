8b150aac58464ee8dd0b6b7db8bd7007a4af8864
Naffy Dharni <naffydharni006@gmail.com>
2024-02-27 23:19:15 +0530

    Add proper hover animation for meta card

--- src/routes/MetaCard.tsx
+++ src/routes/MetaCard.tsx
@@ -1,8 +1,8 @@
 import { Card, CardHeader, Image } from '@nextui-org/react'
 
 export const MetaCard = () => (
-	<Card className='h-[250px] group'>
-		<CardHeader className='absolute z-10 top-0 flex-col h-full justify-end !items-start hidden group-hover:flex transform transition-all ease-in-out duration-1000 group-hover:backdrop-blur-sm group-hover:bg-black/30'>
+	<Card className='h-[250px] group hover:drop-shadow-lg hover:scale-105'>
+		<CardHeader className='absolute z-10 top-0 flex-col h-full justify-end !items-start opacity-0 transform transition-opacity ease-in-out delay-150 duration-1000 group-hover:opacity-100 group-hover:backdrop-blur-sm bg-black/30 rounded-xl'>
 			<p className='text-tiny text-white/60 uppercase font-bold'>
 				What to watch
 			</p>
@@ -13,8 +13,8 @@ export const MetaCard = () => (
 		<Image
 			removeWrapper
 			alt='Card background'
-			className='z-0 w-full h-full object-cover group-hover:grayscale'
-			src='https://cdn.myanimelist.net/images/manga/3/292556.jpg'
+			className='z-0 w-full h-full object-cover group-hover '
+			// src='https://cdn.myanimelist.net/images/manga/3/292556.jpg'
 		/>
 	</Card>
 )
